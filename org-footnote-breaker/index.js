509b4946102928fb62e6786e99ba0984dd0501a0
LiquidZulu <liquidzulu@pm.me>
2021-05-11 21:31:02 +0100

    now with large lines

--- org-footnote-breaker/index.js
+++ org-footnote-breaker/index.js
@@ -1,43 +1,46 @@
 const fs = require('fs')
 const lipsum = require('./lipsum.js')
 
-let orgfile = '* Wrapper\n'
-const fnarr = (()=>{
+let orgfile = 'Travel to [[Footnotes]]\n* Wrapper\n'
+const fnarr = (() => {
     // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     function shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
+        var currentIndex = array.length,
+            temporaryValue,
+            randomIndex
 
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
-
             // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex -= 1
 
             // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
+            temporaryValue = array[currentIndex]
+            array[currentIndex] = array[randomIndex]
+            array[randomIndex] = temporaryValue
         }
 
-        return array;
+        return array
     }
 
     let arr = []
-    for(let i=1; i<lipsum.length+1; i++){
+    for (let i = 1; i < lipsum.length + 1; i++) {
         arr.push(i)
     }
     return shuffle(arr)
 })()
 
-for(let i=0; i<lipsum.length; i++){
-    orgfile += `${lipsum[i]}[fn:${fnarr[i]}]\n`
+for (let i = 0; i < lipsum.length; i++) {
+    orgfile += `${lipsum[i]}[fn:${fnarr[i]}]${Math.random() < 0.2 ? '\n' : ' '}`
 }
 
-orgfile += '\n* Footnotes\n'
+orgfile += '\n\n* Footnotes\n'
 
-for(let i=0; i<lipsum.length; i++){
-    orgfile += `[fn:${fnarr[i]}]\nSome citation text, possibly with some names, who knows, I just need the space with some randomisation ${Math.random()} in case emacs checks for similar lines\n\n`
+for (let i = 0; i < lipsum.length; i++) {
+    orgfile += `[fn:${
+        fnarr[i]
+    }]\nSome citation text, possibly with some names, who knows, I just need the space with some randomisation ${Math.random()} in case emacs checks for similar lines\n\n`
 }
 
-fs.writeFile('./broken-footnotes.org',orgfile,console.log)
+fs.writeFile('./broken-footnotes.org', orgfile, console.log)
