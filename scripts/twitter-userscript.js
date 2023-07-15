8f3b2324b5c1b6bb18b45ed7c34cfe54a2f066a6
LiquidZulu <liquidzulu@pm.me>
2023-07-15 17:15:59 +0100

    Update twitter-userscript.js

--- scripts/twitter-userscript.js
+++ scripts/twitter-userscript.js
@@ -1,19 +1,36 @@
+// ==UserScript==
+// @name         fix-twitter-dm-blueshit
+// @namespace    Twitter
+// @version      0.1
+// @description  Remove subscribe to blue warning from twitter DMs
+// @author       LiquidZulu
+// @match        https://twitter.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @grant        none
+// ==/UserScript==
+
 (() => {
   const removeThatDamnedBox = () => {
-    const spans = document.getElementsByTagName("span");
-    let theSpan = null;
+    try {
+      const spans = document.getElementsByTagName("span");
+      let theSpan = null;
 
-    for (let i = 0; i < spans.length && !theSpan; ++i) {
-      if (
-        spans[i].innerHTML.toLowerCase().startsWith("get verified to message")
-      ) {
-        theSpan = spans[i];
+      for (let i = 0; i < spans.length && !theSpan; ++i) {
+        if (
+          spans[i].innerHTML.toLowerCase().startsWith("get verified to message")
+        ) {
+          theSpan = spans[i];
+        }
       }
-    }
 
-    theSpan.parentElement.parentElement.parentElement
-      .querySelector('[role="button"]')
-      .click();
+      theSpan.parentElement.parentElement.parentElement
+        .querySelector('[role="button"]')
+        .click();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      console.log("Removed the blueshit");
+    }
   };
 
   setInterval(() => {
